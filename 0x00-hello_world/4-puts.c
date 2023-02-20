51b80191c0e1767c1439351b21d6338f87b30f71
bimex <haleembimex30@gmail.com>
2023-02-20 10:59:32 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: when right its always 0
+ */
+
+int main(void)
+{
+	 puts ("\"Programming is like building a multilingual puzzle");
+	         return (0);
+}
